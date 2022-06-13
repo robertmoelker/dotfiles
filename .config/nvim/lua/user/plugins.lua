45a73a00f5e9c948123b3c37ebf69416067db86a
Robert Moelker <rmoelker@gmail.com>
2022-06-13 23:03:35 +0200

    Enable copilot + auto format on plugins

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -1,6 +1,6 @@
-local status_ok, packer = pcall(require, 'packer')
+local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-   return
+	return
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins lua file
@@ -12,62 +12,60 @@ vim.cmd([[
 ]])
 
 return packer.startup(function()
-  use 'wbthomason/packer.nvim'
-  use 'nvim-lua/plenary.nvim'
-  use 'nvim-telescope/telescope.nvim'
-  use 'zane-/cder.nvim' -- used w/ telescope
-  use 'nvim-lualine/lualine.nvim'
-  use 'kyazdani42/nvim-web-devicons'
-  use 'EdenEast/nightfox.nvim'
-  use 'Mofiqul/vscode.nvim'
+	use("wbthomason/packer.nvim")
+	use("nvim-lua/plenary.nvim")
+	use("nvim-telescope/telescope.nvim")
+	use("zane-/cder.nvim") -- used w/ telescope
+	use("nvim-lualine/lualine.nvim")
+	use("kyazdani42/nvim-web-devicons")
+	use("EdenEast/nightfox.nvim")
 
+	use("akinsho/toggleterm.nvim")
+	use("windwp/nvim-autopairs")
+	use("kyazdani42/nvim-tree.lua")
+	use("tpope/vim-commentary")
+	use("folke/which-key.nvim")
 
-  use 'akinsho/toggleterm.nvim'
-  use 'windwp/nvim-autopairs'
-  use 'kyazdani42/nvim-tree.lua'
-  use 'tpope/vim-commentary'
-  use 'folke/which-key.nvim'
+	-- Autocomplete plugins
+	use("neovim/nvim-lspconfig")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-cmdline")
+	use("hrsh7th/nvim-cmp")
+	use("onsails/lspkind.nvim")
 
-  -- Autocomplete plugins
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/nvim-cmp'
-  use 'onsails/lspkind.nvim'
-  
-  -- Snippet plugins
-  use 'hrsh7th/cmp-vsnip'
-  use 'hrsh7th/vim-vsnip'
+	-- Snippet plugins
+	use("hrsh7th/cmp-vsnip")
+	use("hrsh7th/vim-vsnip")
 
-   -- Formatting & linters
-  use 'jose-elias-alvarez/null-ls.nvim'
+	-- Formatting & linters
+	use("jose-elias-alvarez/null-ls.nvim")
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
 
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
-  }
+	use({
+		"akinsho/bufferline.nvim",
+		tag = "v2.*",
+		requires = "kyazdani42/nvim-web-devicons",
+	})
 
-  use {
-    'akinsho/bufferline.nvim', 
-    tag = "v2.*", 
-    requires = 'kyazdani42/nvim-web-devicons'
-  }
+	use("github/copilot.vim")
+	-- -- Github copilot lua edition
+	use({
+		"zbirenbaum/copilot.lua",
+		event = "InsertEnter",
+		config = function()
+			vim.schedule(function()
+				require("copilot").setup()
+			end)
+		end,
+	})
 
-  -- -- Github copilot lua edition
-  -- use {
-  --   "zbirenbaum/copilot.lua",
-  --   event = {"VimEnter"},
-  --   config = function()
-  --     vim.defer_fn(function()
-  --       require("copilot").setup()
-  --     end, 100)
-  --   end,
-  -- }  
-
-  -- use {
-  --   "zbirenbaum/copilot-cmp",
-  --   after = { "copilot.lua", "nvim-cmp" },
-  -- }
+	use({
+		"zbirenbaum/copilot-cmp",
+		after = { "copilot.lua", "nvim-cmp" },
+	})
 end)

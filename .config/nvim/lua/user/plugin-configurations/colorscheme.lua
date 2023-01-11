13e9cf58a54daa23b7ea80b2d016c3a661ebb076
Robert Moelker <rmoelker@gmail.com>
2023-01-11 09:47:08 +0100

    Lets see if i can style the nvim

--- .config/nvim/lua/user/plugin-configurations/colorscheme.lua
+++ .config/nvim/lua/user/plugin-configurations/colorscheme.lua
@@ -3,7 +3,7 @@
 -- https://github.com/EdenEast/nightfox.nvim
 --
 -- >> DARK
--- nordfox 
+-- nordfox
 -- nightfox
 -- duskfox
 -- terafox
@@ -12,4 +12,7 @@
 -- dayfox
 -- dawnfox
 
-vim.cmd("colorscheme nightfox")
+-- vim.cmd("colorscheme nightfox")
+vim.cmd("colorscheme duskfox")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })

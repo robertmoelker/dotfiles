35105354a4a5dfa3253e0704e017e9df6737cccf
Robert Moelker <rmoelker@gmail.com>
2024-07-23 17:46:06 +0200

    feat: setup the LSP

--- .config/nvim-lazy/lua/plugins/mason.lua
+++ .config/nvim-lazy/lua/plugins/mason.lua
@@ -1,13 +1,23 @@
 return {
   {
 		"williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
     "williamboman/mason-lspconfig.nvim",
+    config = function() 
+      require("mason-lspconfig").setup()
+    end
+  },
+  {
     "neovim/nvim-lspconfig",
     config = function()
-			require("lspconfig").tsserver.setup()
-			require("lspconfig").lua_ls.setup()
-			require("lspconfig").phpactor.setup()
-			require("lspconfig").gopls.setup()
-    end,
+			require("lspconfig").tsserver.setup{}
+			require("lspconfig").lua_ls.setup{}
+			require("lspconfig").phpactor.setup{}
+			require("lspconfig").gopls.setup{}
+    end
   },
 }

ecd96c87beaebf9c19cdda4d20492c70e06d6c61
Robert Moelker <rmoelker@gmail.com>
2025-06-25 09:40:53 +0200

    chore: set transparency in the terminal

--- .config/nvim-lazy/lua/plugins/colorscheme.lua
+++ .config/nvim-lazy/lua/plugins/colorscheme.lua
@@ -1,45 +1,46 @@
 return {
-	{
-		"rebelot/kanagawa.nvim",
-		lazy = false,
-		config = function()
-			require("kanagawa").setup({
-				colors = {
-					theme = {
-						all = {
-							ui = {
-								bg_gutter = "none",
-							},
-						},
-					},
-				},
-				overrides = function()
-					return {
-						-- update kanagawa to handle new treesitter highlight captures
-						["@string.regexp"] = { link = "@string.regex" },
-						["@variable.parameter"] = { link = "@parameter" },
-						["@exception"] = { link = "@exception" },
-						["@string.special.symbol"] = { link = "@symbol" },
-						["@markup.strong"] = { link = "@text.strong" },
-						["@markup.italic"] = { link = "@text.emphasis" },
-						["@markup.heading"] = { link = "@text.title" },
-						["@markup.raw"] = { link = "@text.literal" },
-						["@markup.quote"] = { link = "@text.quote" },
-						["@markup.math"] = { link = "@text.math" },
-						["@markup.environment"] = { link = "@text.environment" },
-						["@markup.environment.name"] = { link = "@text.environment.name" },
-						["@markup.link.url"] = { link = "Special" },
-						["@markup.link.label"] = { link = "Identifier" },
-						["@comment.note"] = { link = "@text.note" },
-						["@comment.warning"] = { link = "@text.warning" },
-						["@comment.danger"] = { link = "@text.danger" },
-						["@diff.plus"] = { link = "@text.diff.add" },
-						["@diff.minus"] = { link = "@text.diff.delete" },
-					}
-				end,
-			})
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    config = function()
+      require("kanagawa").setup({
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = "none",
+              },
+            },
+          },
+        },
+        transparent = true,
+        overrides = function()
+          return {
+            -- update kanagawa to handle new treesitter highlight captures
+            ["@string.regexp"] = { link = "@string.regex" },
+            ["@variable.parameter"] = { link = "@parameter" },
+            ["@exception"] = { link = "@exception" },
+            ["@string.special.symbol"] = { link = "@symbol" },
+            ["@markup.strong"] = { link = "@text.strong" },
+            ["@markup.italic"] = { link = "@text.emphasis" },
+            ["@markup.heading"] = { link = "@text.title" },
+            ["@markup.raw"] = { link = "@text.literal" },
+            ["@markup.quote"] = { link = "@text.quote" },
+            ["@markup.math"] = { link = "@text.math" },
+            ["@markup.environment"] = { link = "@text.environment" },
+            ["@markup.environment.name"] = { link = "@text.environment.name" },
+            ["@markup.link.url"] = { link = "Special" },
+            ["@markup.link.label"] = { link = "Identifier" },
+            ["@comment.note"] = { link = "@text.note" },
+            ["@comment.warning"] = { link = "@text.warning" },
+            ["@comment.danger"] = { link = "@text.danger" },
+            ["@diff.plus"] = { link = "@text.diff.add" },
+            ["@diff.minus"] = { link = "@text.diff.delete" },
+          }
+        end,
+      })
 
-			vim.cmd("colorscheme kanagawa-dragon")
-		end,
-	},
+      vim.cmd("colorscheme kanagawa-dragon")
+    end,
+  },
 }

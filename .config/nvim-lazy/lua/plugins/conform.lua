69515e127d60cec4683beb65c5f328a810968c9b
Robert Moelker <rmoelker@gmail.com>
2025-06-25 09:40:08 +0200

    refactor: cleanup copilot - use rg instead of fd filesearch

--- .config/nvim-lazy/lua/plugins/conform.lua
+++ .config/nvim-lazy/lua/plugins/conform.lua
@@ -1,39 +1,40 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		event = { "BufWritePre" },
-		cmd = { "ConformInfo" },
-		keys = {
-			{
-				"<leader>r",
-				function()
-					--vim.api.nvim_buf_write(0, nil, nil)
-					require("conform").format({ async = true }, function()
-						print("Formatted.")
-					end)
-				end,
-				mode = "",
-				desc = "Format buffer",
-			},
-		},
-		opts = {
-			formatters_by_ft = {
-				lua = { "stylua" },
-				go = { "gofmt" },
-				javascript = { "eslint_d" },
-				typescript = { "eslint_d" },
-				vue = { "eslint_d" },
-				json = { "prettierd" },
-				xml = { "xmllint" },
-				css = { "stylelint" },
-				scss = { "stylelint" },
-				php = { "php_cs_fixer" },
-			},
-			notify_on_error = true,
-			notify_no_formatters = true,
-			default_format_opts = {
-				lsp_format = "fallback",
-			},
-		},
-	},
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        "<leader>r",
+        function()
+          require("conform").format({ async = true }, function()
+            print("Formatted.")
+          end)
+        end,
+        mode = "",
+        desc = "Format buffer",
+      },
+    },
+    opts = {
+      formatters_by_ft = {
+        bicep = { "bicep" },
+        lua = { "stylua" },
+        go = { "gofmt" },
+        javascript = { "eslint_d" },
+        typescript = { "biome", "biome-check", "biome-organize-imports" },
+        vue = { "eslint_d" },
+        json = { "prettierd" },
+        xml = { "xmllint" },
+        css = { "stylelint" },
+        scss = { "stylelint" },
+        php = { "php_cs_fixer" },
+      },
+      notify_on_error = true,
+      notify_no_formatters = true,
+      format_on_save = {
+        lsp_format = "fallback",
+        timeout_ms = 1000,
+      }
+    },
+  },
 }
--- .config/nvim-lazy/lua/plugins/telescope.lua
+++ .config/nvim-lazy/lua/plugins/telescope.lua
@@ -1,13 +1,36 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		keys = {
-			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-			{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Search for string" },
-			{ "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Find buffer" },
-		},
-	},
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      defaults = {
+        vimgrep_arguments = {
+          'rg',
+          '--color=never',
+          '--no-heading',
+          '--with-filename',
+          '--line-number',
+          '--column',
+          '--smart-case',
+        }
+      },
+      pickers = {
+        find_files = {
+          find_command = {
+            'rg',
+            '--files',
+            '--hidden',
+            '--glob', '!.git/',
+          },
+        },
+      },
+    },
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "Search for string" },
+      { "<leader>fb", "<cmd>Telescope buffers<CR>",    desc = "Find buffer" },
+    },
+  },
 }

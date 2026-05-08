e797ba3e95dff9443ea5c9e60c0845beee453bad
Robert Moelker <rmoelker@gmail.com>
2026-05-09 00:05:01 +0200

    fix: conform timeout & biome err

--- .config/nvim-lazy/lua/plugins/conform.lua
+++ .config/nvim-lazy/lua/plugins/conform.lua
@@ -17,11 +17,12 @@ return {
     },
     opts = {
       formatters_by_ft = {
-        bicep = { "bicep" },
+        -- bicep = { "bicep" },
+        -- bicep = false,
         lua = { "stylua" },
         go = { "gofmt" },
         javascript = { "eslint_d" },
-        typescript = { "biome", "biome-check", "biome-organize-imports" },
+        typescript = { "biome-check" },
         vue = { "eslint_d" },
         json = { "prettierd" },
         xml = { "xmllint" },
@@ -29,12 +30,37 @@ return {
         scss = { "stylelint" },
         php = { "php_cs_fixer" },
       },
+      -- formatters = {
+      --   bicep = function()
+      --     -- print("filename", vim.api.nvim_buf_get_name(0))
+      --     return {
+      --       exe = "bicep",
+      --       args = { "format", "-" },
+      --       stdin = true,
+      --       -- stdout = false,
+      --     }
+      --   end,
+      -- },
+
+      formatters = {
+        ["biome-organize-imports"] = {
+          args = {
+            "check",
+            "--write",
+            "--formatter-enabled=false",
+            "--linter-enabled=false",
+            "--assists-enabled=true",
+            "--stdin-file-path",
+            "$FILENAME",
+          },
+        },
+      },
       notify_on_error = true,
       notify_no_formatters = true,
       format_on_save = {
         lsp_format = "fallback",
-        timeout_ms = 1000,
-      }
+        timeout_ms = 3000,
+      },
     },
   },
 }
